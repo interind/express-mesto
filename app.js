5daaafb5a61c9777bfceeeaed1d00b731aeb3baa
interind <interind@yandex.ru>
2020-11-24 17:38:58 +0300

    transferring files React

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const express = require('express');
 
-
 const PORT = process.env.PORT || 3000;
 const { BASE_PATH = `http://localhost:${PORT}` } = process.env;
 
 const app = express();
 
-app.listen(PORT, ()=> {
-  console.log(`the server is running at ${BASE_PATH}`);
-})
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
+  console.log(`'the server is running at' ${BASE_PATH}`);
+});
