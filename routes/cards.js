0542d64d3887a8aee04de465cbed4f6eb68ffde4
interind <interind@yandex.ru>
2020-11-25 16:19:05 +0300

    User

--- routes/cards.js
+++ routes/cards.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const router = require('express').Router();
 
-const cards = [];
-
 router.get('/cards', (req, res) => {
   // if (!cards[req.params.id]) {
   //   res.send({ error: 'Такого пользователя нет' });
