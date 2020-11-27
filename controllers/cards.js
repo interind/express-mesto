0d70532a5314626f29dec187477e2de8c64a6993
interind <interind@yandex.ru>
2020-11-27 10:38:20 +0300

    helpers for file

---
+++ controllers/cards.js
@@ -0,0 +1,15 @@
+// import getDataFromFile from '../helpers/files';
+
+// const path = require('path');
+
+// const dataPath = path.join(__dirname, '..', 'data', 'cards.json');
+
+// const getCards = ((req, res) => {
+//   getDataFromFile(dataPath)
+//     .then((cards) => {
+//       res.status(200).send(cards);
+//     })
+//     .catch((err) => res.status(500).send({ message: err.message }));
+// });
+
+// module.exports = getCards;
