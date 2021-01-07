654f3554fa88e7eb3101889818c40b977e282693
Aleksandr <interind@yandex.ru>
2021-01-07 20:20:24 +0300

    select password

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
 const auth = require('../middlewares/auth.js');
+
 const {
-  createUser,
-  login,
   getUsers,
   getUser,
   updateUser,
@@ -12,29 +10,7 @@ const {
 
 router.get('/users', getUsers);
 router.get('/users/:userId', getUser);
-router.post(
-  '/signup',
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().required(),
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(6),
-    }),
-  }),
-  createUser,
-);
-router.post(
-  '/signin',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(6),
-    }),
-  }),
-  login,
-);
+
 router.patch('/users/me', auth, updateUser);
 router.patch('/users/me/avatar', auth, updateUserAvatar);
 
