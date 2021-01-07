74dc83da4b60c3eda062a8d42a9943b9b9311c76
Aleksandr <interind@yandex.ru>
2021-01-07 12:39:10 +0300

    useUnifiedTopology

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ async function start() {
   try {
     await mongoose.connect(mongodbUrl, {
       useNewUrlParser: true,
+      useUnifiedTopology: true,
       useCreateIndex: true,
       useFindAndModify: false,
     });
